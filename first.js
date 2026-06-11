d9422b65a0073453e60d404e68a5bbfde3e4b228
komal-Chaudhary-09 <komalchoudhary260408@gmail.com>
2026-06-11 10:53:51 +0530

    practice

--- first.js
+++ first.js
@@ -1,6 +1,15 @@
-let num = 1;
-console.log(typeof num);
+function Car(model,colour,price){
+    this.model = model;
+    this.colour = colour;
+    this.price = price
 
-second = Boolean(num);
-console.log(typeof second)
-console.log(second)
+ }
+
+ let car1 = new Car("swift" ,"red" ,900);
+ car1.size ="big";
+ 
+
+  car1.details = function(){
+    return this.model +" " + this.colour;
+  }
+  console.log(car1.details());
